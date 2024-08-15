4c1d99859e0a6ce54ceae80c8b68cdd0aa7472f9
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2024-08-15 10:37:00 -0300

    atividade completa organizada

--- src/main/java/controller/Base.java
+++ src/main/java/controller/Base.java
@@ -6,47 +6,43 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Base {
-   static Scanner scanner = new Scanner(System.in);
-   static Conteudos conteudos = new Conteudos();
-   static HashMap<Integer,String>listaConteudos = new HashMap<>();
-   static Login login = new Login();
-    public void menuInicial(){
+    static Scanner scanner = new Scanner(System.in);
+    static Conteudos conteudos = new Conteudos();
+    static HashMap<Integer, String> listaConteudos = new HashMap<>();
+    static Login login = new Login();
 
-        int escolha =0;
-
-        while (escolha!=4){
+    public void menuInicial() {
+        int escolha = 0;
+        while (escolha != 4) {
             System.out.println("Menu incial: ");
             System.out.println("1. Login");
             System.out.println("2. Listar conteudos");
             System.out.println("4. Sair");
             escolha = scanner.nextInt();
-            switch (escolha){
-                case 1 : menuPos();
-                continue;
-                case 2 :
+            switch (escolha) {
+                case 1:
+                    menuPos();
+                    continue;
+                case 2:
                     System.out.println(listaConteudos.toString());
                     break;
-                case 4 :
+                case 4:
                     System.out.println("voce saiu");
                     break;
                 default:
                     System.out.println("opcao invalida");
                     break;
-
             }
-
         }
-
     }
 
-    private void menuPos(){
-        int escolha=0, idTemp;
+    private void menuPos() {
+        int escolha = 0, idTemp;
         String alteracao;
-
-        try{
+        try {
             System.out.println("digite seu usuario e senha");
-            if (login.login(scanner.next(), scanner.next())){
-                while (escolha!= 5){
+            if (login.login(scanner.next(), scanner.next())) {
+                while (escolha != 5) {
                     System.out.println("Menu incial: ");
                     System.out.println("1. Criar conteudo");
                     System.out.println("2. Listar conteudo");
@@ -54,29 +50,25 @@ public class Base {
                     System.out.println("4. Excluir conteudo");
                     System.out.println("5. Logout");
                     escolha = scanner.nextInt();
-                    switch (escolha){
+                    switch (escolha) {
                         case 1 -> {
                             System.out.println("digite o id do conteudo");
                             conteudos.setId(scanner.nextInt());
                             System.out.println("digite a descricao do conteudo");
-                            alteracao=scanner.next();
-                            conteudos.setDetalhes(alteracao);
-                            criador(conteudos.getId(),conteudos.getDetalhes());
-
+                            conteudos.setDetalhes(scanner.next());
+                            criador(conteudos.getId(), conteudos.getDetalhes());
                         }
-                        case 2 ->{
-                                System.out.println(listaConteudos.toString());
+                        case 2 -> {
+                            System.out.println(listaConteudos.toString());
                         }
-
-                        case 3 ->{
+                        case 3 -> {
                             System.out.println("digite o id do conteudo que quer mudar");
                             idTemp = scanner.nextInt();
                             System.out.println("digite a alteracao");
                             alteracao = scanner.next();
-                            alterador(idTemp,alteracao);
-
+                            alterador(idTemp, alteracao);
                         }
-                        case 4 ->{
+                        case 4 -> {
                             System.out.println("digite o id do conteudo que quer remover");
                             idTemp = scanner.nextInt();
                             removedor(idTemp);
@@ -84,51 +76,47 @@ public class Base {
                         case 5 -> {
                             System.out.println("voce deslogou");
                         }
-                        default-> {
+                        default -> {
                             System.out.println("opcao invalida");
                         }
                     }
                 }
             }
-
-
-
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             e.getMessage();
         }
-
-
     }
-    private HashMap<Integer,String> criador(int id, String detalhes){
+
+    private HashMap<Integer, String> criador(int id, String detalhes) {
         try {
-            listaConteudos.put(id,detalhes);
+            listaConteudos.put(id, detalhes);
             return listaConteudos;
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             e.getMessage();
             return null;
         }
-
     }
-    private void alterador(int idTemp, String alteracao){
+
+    private void alterador(int idTemp, String alteracao) {
         try {
-           if (listaConteudos.containsKey(idTemp))
-               listaConteudos.replace(idTemp,alteracao);
-           else {
-               System.out.println("conteudo nao existe");
-           }
-        }catch (RuntimeException e){
+            if (listaConteudos.containsKey(idTemp))
+                listaConteudos.replace(idTemp, alteracao);
+            else {
+                System.out.println("conteudo nao existe");
+            }
+        } catch (RuntimeException e) {
             e.getMessage();
         }
-
     }
-    private void removedor(int idTemp){
-        try{
+
+    private void removedor(int idTemp) {
+        try {
             if (listaConteudos.containsKey(idTemp))
                 listaConteudos.remove(idTemp);
             else {
                 System.out.println("conteudo nao exite");
             }
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             e.getMessage();
         }
     }
--- src/main/java/controller/Login.java
+++ src/main/java/controller/Login.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Login {
     private String usuario;
     private String senha;
+
     public String getUsuario() {
         return usuario;
     }
@@ -24,19 +25,19 @@ public class Login {
         this.senha = senha;
     }
 
-    public boolean login(String usuario, String senha){
+    public boolean login(String usuario, String senha) {
         setUsuario("eduardo");
         setSenha("2005");
         try {
 
-            if (!Objects.equals(this.usuario, usuario) || !Objects.equals(this.senha, senha)){
+            if (!Objects.equals(this.usuario, usuario) || !Objects.equals(this.senha, senha)) {
                 System.out.println("usuario e/ou senha invalido");
                 return false;
             } else {
-               return true;
+                return true;
 
             }
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             e.getMessage();
             return false;
         }
